5ef2d354951db912b5a9b459e341a345e68d1537
Ángel Arias <angadominguez96@gmail.com>
2023-08-07 18:13:14 +0200

    iteration 5 done

--- routes/drones.js
+++ routes/drones.js
@@ -73,9 +73,14 @@ router.post('/drones/:id/edit', async (req, res, next) => {
   }
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post('/drones/:id/delete', async (req, res, next) => {
   // Iteration #5: Delete the drone
-  // ... your code here
+  try {
+    await Drone.findByIdAndDelete(req.params.id)
+    res.redirect('/drones')
+  } catch (error) {
+    next(error)
+  }
 });
 
 module.exports = router;
